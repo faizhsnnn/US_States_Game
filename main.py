9b23a037515fd8d40e32f2cc8fdd65ca3c58053f
Faiz <hasanfaiz2107@gmail.com>
2026-01-14 22:17:58 +0530

    Refactor missing states logic using list comprehension

--- main.py
+++ main.py
@@ -23,13 +23,11 @@ while len(guessed_states) < 50:
     answer_state = answer_state.title()
 
     if answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
+        
 
         new_data = pandas.DataFrame(missing_states)
-        new_data.to_csv("states_to_learn.csv", index=False)
+        new_data.to_csv("states_to_learn.csv")
         break
 
     if answer_state in all_states and answer_state not in guessed_states:
